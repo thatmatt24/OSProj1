2fa2233fa2adf095e0fc1b887366822cdcb9456a
mattmcmahon <matt.mcmahon424@gmail.com>
2019-05-02 18:25:24 -0600

    comments

--- ServerSide.java
+++ ServerSide.java
@@ -16,29 +16,18 @@ public class ServerSide {
 	public static LinkedList<Integer> guesses;
 	public static String password = ""; 
 
+	// Receives user input for number of clients and servers as well as size of buffer
+	// 		also gets the password from the user to be verified by the client(s)
 	public static void main(String[] args) throws Exception {
-		// Object of a class that has both produce()
-		// and consume() methods
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Enter number of clients: ");
 		clsize = scanner.nextInt();
 		System.out.println();
 
-		System.out.print("Enter number of servers (Maximum of 9): ");
+		System.out.print("Enter number of servers: ");
 		prosize = scanner.nextInt();
 		System.out.println();
-		if (prosize > 9) {
-			System.out.println("Maximum number of servers exceeded!\nSetting number " + "of servers to 9...\n");
-			prosize = 9;
-		}
-
-		// int[] sock_list = new int[prosize];
-		// int[] port_list = { 1337, 1859, 3000, 3002, 3030, 3128, 3306, 3333, 3621 };
-
-		// for (int i = 0; i < sock_list.length; i++) {
-		// sock_list[i] = port_list[i];
-		// }
 
 		System.out.print("Enter size of buffer: ");
 		buff = scanner.nextInt();
@@ -48,10 +37,14 @@ public class ServerSide {
 		password = scanner.nextLine();
 		System.out.println();
 
+		// creates a socket using port 59898 (which is an open, general purpose port, mainly TCP use though)
 		try (var listener = new ServerSocket(59898)) {
 			System.out.println("The server is running...");
+			// creates a thread pool which is the limit of the number of servers allowed, created by user "prosize"
 			var pool = Executors.newFixedThreadPool(prosize);
 			while (true) {
+				// calls the Server class and starts listening on port 59898
+					// essentially starts the server
 				pool.execute(new Server(listener.accept()));
 			}
 		}
@@ -60,6 +53,8 @@ public class ServerSide {
 	private static class Server implements Runnable {
 		private Socket socket;
 
+		// contructor for socket, which defines the global 
+		// 		socket as the local one to be used here
 		Server(Socket socket) {
 			this.socket = socket;
 		}
@@ -71,9 +66,12 @@ public class ServerSide {
 			int guess = 0; 
 
 			try {
+				// always while true so server will continue to run when a client accesses it;
+				// 		so the server can spawn threads continuously when a client is created
 				while (true) {
 					synchronized (this) {
 
+						// bounded buffer; the server can only handle the "buff" size of actions
 						while (guesses.size() == buff) {
 							wait();
 						}
@@ -83,13 +81,15 @@ public class ServerSide {
 						while (in.hasNextLine()) {
 							// insert Server action to client here
 							if (in.nextLine() == password) {
-								
+
 								out.println("ACCESS GRANTED");
 							} else {
 								guesses.add(guess++);
-								out.println("ACCESS DENIED\nTry again..(you have " + (buff - guesses.size()) + " left)");
+								out.println(
+										"ACCESS DENIED\nTry again..(you have " + (buff - guesses.size()) + " left)");
 							}
 						}
+						// notifies the other threads that they can continue
 						notify();
 						// sleep for 3 seconds
 						Thread.sleep(300);
